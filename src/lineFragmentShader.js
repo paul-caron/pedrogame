957c1e96bf843dcd9bd3538d864c15e73f61dcf9
Paul Caron <45680511+paul-caron@users.noreply.github.com>
2025-02-28 22:39:39 -0600

    Update lineFragmentShader.js

--- src/lineFragmentShader.js
+++ src/lineFragmentShader.js
@@ -1,6 +1,5 @@
-precision mediump float;
-varying vec2 v_texCoord;
-uniform sampler2D u_texture;
+varying vec4 v_color;  // The color passed from the vertex shader
+
 void main() {
-    gl_FragColor = texture2D(u_texture, v_texCoord) ;
+    gl_FragColor = v_color;  // Output the color for the line
 }
